34acf00ad434ab81ad5cc00f5ea57d7bb00bd1b5
Tran Quoc Kiet <kiettran130905@gmail.com>
2026-03-12 08:04:51 +0700

    Add project files.

---
+++ TranQuocKiet_2123110364/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace TranQuocKiet_2123110364
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
