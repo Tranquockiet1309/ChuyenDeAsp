2963fd8764d04d018c8b55890cb2d006be342997
Tran Quoc Kiet <kiettran130905@gmail.com>
2026-03-12 11:20:58 +0700

    Buoi 1

---
+++ TranQuocKiet_2123110364/Controllers/CategoryController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TranQuocKiet_2123110364.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        // danh sách lưu tạm
+        private static List<object> categories = new List<object>();
+        [HttpGet]
+        // GET: api/Category
+        public IActionResult GetCategories()
+        {
+            return Ok(categories);
+        }
+        [HttpPost]
+        // POST: api/Category
+        public IActionResult CreateCategory([FromBody] object category)
+        {
+            categories.Add(category);
+            return Ok(category);
+        }
+        [HttpPut("{id}")]
+        // PUT: api/Category/{id}
+        public IActionResult UpdateCategory(int id, [FromBody] object category)
+        {
+            if (id < 0 || id >= categories.Count)
+            {
+                return NotFound();
+            }
+            categories[id] = category;
+            return Ok(category);
+        }
+        [HttpDelete("{id}")]
+        // DELETE: api/Category/{id}
+        public IActionResult DeleteCategory(int id)
+        {
+            if (id < 0 || id >= categories.Count)
+            {
+                return NotFound();
+            }
+            var deletedCategory = categories[id];
+            categories.RemoveAt(id);
+            return Ok(deletedCategory);
+        }
+    }
+}
---
+++ TranQuocKiet_2123110364/Controllers/ProductController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace TranQuocKiet_2123110364.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        // danh sách lưu tạm
+        private static List<object> products = new List<object>();
+
+        // GET: api/Product
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return Ok(products);
+        }
+
+        // POST: api/Product
+        [HttpPost]
+        public IActionResult CreateProduct([FromBody] object product)
+        {
+            products.Add(product);
+            return Ok(product);
+        }
+        [HttpPut("{id}")]
+        // PUT: api/Product/{id}
+        public IActionResult UpdateProduct(int id, [FromBody] object product)
+        {
+            if (id < 0 || id >= products.Count)
+            {
+                return NotFound();
+            }
+            products[id] = product;
+            return Ok(product);
+        }
+        [HttpDelete("{id}")]
+        // DELETE: api/Product/{id}
+        public IActionResult DeleteProduct(int id)
+        {
+            if (id < 0 || id >= products.Count)
+            {
+                return NotFound();
+            }
+            var deletedProduct = products[id];
+            products.RemoveAt(id);
+            return Ok(deletedProduct);
+        }
+
+    }
+}
